6ba51250fb261049920b0a98602e4af3fc1b904f
Yuliia Mohylna <mohylna.yuliia@gmail.com>
2022-04-08 16:32:20 +0300

    added test case when user borrow a book and book is removed from library

--- library/src/main/java/library/entity/BookEntity.java
+++ library/src/main/java/library/entity/BookEntity.java
@@ -20,4 +20,6 @@ public class BookEntity {
 
     private String name;
 
+    private boolean exists;
+
 }
--- library/src/test/java/library/service/impl/LibraryServiceTestImpl.java
+++ library/src/test/java/library/service/impl/LibraryServiceTestImpl.java
@@ -4,13 +4,18 @@ import library.Application;
 import library.entity.BookEntity;
 import library.repository.LibraryRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -18,6 +23,7 @@ public class LibraryServiceTestImpl {
 
     private static final String FIRST_BOOK_NAME = "First Book";
     private static final int FIRST_BOOK_ID = 1;
+    public static final int FIRST_USER_ID = 1;
 
     @Autowired
     private LibraryServiceImpl libraryService;
@@ -25,24 +31,48 @@ public class LibraryServiceTestImpl {
     @Autowired
     private LibraryRepository libraryRepository;
 
+    @Autowired
+    private UserBookRepository userBookRepository;
+
+    @BeforeEach
+    public void setup() {
+        libraryRepository.deleteAll();
+    }
+
     @Test
     public void testWhenNoBooksInLibrary() {
-        libraryRepository.deleteAll();
         Assertions.assertEquals(0, ((Collection<?>) libraryRepository.findAll()).size());
 
         Assertions.assertEquals(0, libraryService.getAllBooks().size());
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void testWhenThereAreBooksInLibrary() {
-        libraryRepository.deleteAll();
         libraryRepository.save(BookEntity.builder().id(FIRST_BOOK_ID).name(FIRST_BOOK_NAME).build());
         Assertions.assertEquals(1, ((Collection<?>) libraryRepository.findAll()).size());
 
         Assertions.assertEquals(1, libraryService.getAllBooks().size());
         Assertions.assertEquals(FIRST_BOOK_NAME, libraryService.getAllBooks().get(0).getName());
+    }
 
-        libraryRepository.deleteAll();
+    @Test
+    @Transactional
+    @Rollback
+    public void testWhenUserCanBorrowBookAndBooksRemovedFromLibrary() {
+        libraryRepository.save(BookEntity.builder().id(FIRST_BOOK_ID).name(FIRST_BOOK_NAME).build());
+        Assertions.assertEquals(1, ((Collection<?>) libraryRepository.findAll()).size());
+
+        libraryService.borrowBook(FIRST_USER_ID, FIRST_BOOK_ID);
+
+        List<BookEntity> books = new ArrayList<>();
+        libraryRepository.findAll().forEach(books::add);
+
+        Assertions.assertFalse(books.get(0).isExists());
+        List<UserBookEntity>userBooks=userBookRepository.findByUserId();
+        Assertions.assertEquals(1, userBooks.size());
+        Assertions.assertEquals(1, userBooks.getBooks().size());
     }
 
 
